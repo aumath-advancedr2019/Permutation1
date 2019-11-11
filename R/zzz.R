477b98147d64e8808b5228bed8e9314ffc92ba30
agomez <adriangomezrepolles@gmail.com>
2019-11-11 10:43:15 +0100

    zzz update

--- R/zzz.R
+++ R/zzz.R
@@ -3,13 +3,13 @@
     cat('Load required: ggplot2','\n')
     library("ggplot2")
   } else {
-    warning('Some functions may require ggplot2')
+    cat('Some functions may require ggplot2')
   }
   if ("magrittr" %in% rownames(installed.packages())){
     cat('Load required: magrittr','\n')
     library("magrittr")
   } else {
-    warning('Some functions may require magrittr')
+    cat('Some functions may require magrittr')
   }
 }
 
