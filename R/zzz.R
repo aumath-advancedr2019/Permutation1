d595d56de0a23396bb92f27cb3241ddb314648b8
SolveigOe <solveig.oestergaard@gmail.com>
2019-11-11 10:52:28 +0100

    Small changes

--- R/zzz.R
+++ R/zzz.R
@@ -3,13 +3,13 @@
     cat('Load required: ggplot2','\n')
     library("ggplot2")
   } else {
-    cat('Some functions may require ggplot2')
+    cat('Some functions may require ggplot2','\n')
   }
   if ("magrittr" %in% rownames(installed.packages())){
     cat('Load required: magrittr','\n')
     library("magrittr")
   } else {
-    cat('Some functions may require magrittr')
+    cat('Some functions may require magrittr','\n')
   }
 }
 
